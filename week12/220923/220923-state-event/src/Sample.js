158a1a8f151777399324818dc032911338136d4c
Junghwa <jeongjwa054@gmail.com>
2022-09-25 15:58:25 +0900

    study: stateClass Component

--- week12/220923/220923-state-event/src/Sample.js
+++ week12/220923/220923-state-event/src/Sample.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Sample = () => {
-    const fashion = ["청바지", "패딩", "반팔", "운동화"];
-    const fashionList = fashion.map(fashion => <li>{fashion}</li>);
+    const [names, setNames] = useState([
+        {id: 1, text: '청바지'},
+        {id: 2, text: '패딩'},
+        {id: 3, text: '반팔'},
+        {id: 4, text: '운동화'}
+    ]);
+    
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5); //새로운 항목을 추가할 떄 사용할id
 
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
+     
+
+    const nameList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
     return(
-        <ul>{fashionList}</ul>
+        <div>
+            <input value={inputText} onChange={onChange}></input>
+            <button onclick={onClick}>추가</button>
+            <ul>
+                {nameList}
+            </ul>
+        </div>
     );
-}
+};
 
-//map
-// let numbers = [1, 2, 3, 4, 5];
-
-// let mul = numbers.map(function(num){
-//     return num*num;
-// })
-// console.log(mul);
-//=> 1,4,9,16,25 map함수는 기존배여로 새로운 배열을 만드는 역할을 한다.
-// const number = [1,2,3,4,5];
-// const result = numbers.map(num => num*num);
-// console.log(result);
-//위의 코드를 ES6문법으로 작성하면 이렇게 된다. 
-//let 대신const, function(){}대신 화살표 함수를 이용한것.
 
 
 export default Sample;
