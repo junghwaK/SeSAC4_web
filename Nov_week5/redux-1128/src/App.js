0e4dddca395db0d8f8e0caae7b04c4f78035bdda
junghwaK <jeonghwa054@gmail.com>
2022-11-28 13:24:45 +0900

    study: store and reducer

--- Nov_week5/redux-1128/src/App.js
+++ Nov_week5/redux-1128/src/App.js
@@ -1,42 +1,53 @@
 import './App.css';
 import { useState } from 'react';
 
+import {createStore} from 'redux';
+//store관리할때 모든 친구들의 최상위.
+import {Provider} from 'react-redux';
+
+function reducer(state, action){
+  return {number: 1};
+}
+
+const store = createStore(reducer);
 function App() {
   const [number, setNumber] = useState(0);
   return (
+    <Provider store={store}>
     <div id="app">
       <h1>number : {number}</h1>
       <Box1 number={number} onIncrease={()=>{setNumber(number+1)}} onDecrease={()=>{setNumber(number-1)}}/>
     </div>
+    </Provider>
   );
 }
 
-const Box1 = (props) =>{
+const Box1 = () =>{
   console.log('Box1');
   return (
     <div>
-      <h2>Box1 컴포넌트 number : {props.number}</h2>
-      <Box2 number={props.number} onIncrease={props.onIncrease} onDecrease={props.onDecrease} />
+      <h2>Box1 컴포넌트 number : </h2>
+      <Box2 />
     </div>
   )
 }
 
-const Box2 = (props) =>{
+const Box2 = () =>{
   console.log('Box2');
   return (
     <div>
-      <h3>Box2 컴포넌트 number : {props.number}</h3>
-      <Box3 number={props.number} onIncrease={props.onIncrease} onDecrease={props.onDecrease} />
+      <h3>Box2 컴포넌트 number : </h3>
+      <Box3 />
     </div>
   )
 }
-const Box3 = (props) =>{
+const Box3 = () =>{
   console.log('Box3');
   return (
     <div>
-      <h4>Box3 컴포넌트 number : {props.number}</h4>
-      <input type="button" value="+" onClick={()=>{props.onIncrease()}} />
-      <input type="button" value="-" onClick={()=>{props.onDecrease()}} />
+      <h4>Box3 컴포넌트 number : </h4>
+      <input type="button" value="+" onClick={()=>{}} />
+      <input type="button" value="-" onClick={()=>{}} />
     </div>
   )
 }
