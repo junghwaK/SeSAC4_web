a71ce2ec682ab4293137ec12cbb67cfc9710ff71
junghwaK <jeonghwa054@gmail.com>
2022-12-05 16:11:54 +0900

    study: ts-function

---
+++ Dec_week1/221205-init/02-function.ts
@@ -0,0 +1,15 @@
+// function 함수이름 (변수명: 타입) : 반환타입 {}
+
+function sum1(a, b) {
+    return a + b;
+}
+console.log( sum1(1, 'a') );
+console.log( sum1(1, null) );
+function sum2( a:number, b:number):number {
+    return a+b;
+}
+console.log( sum2(1,2) );
+function sum3( a:number, b?:number):number {
+    return a + b;
+}
+console.log( "sum3 : ", sum3(1) );
